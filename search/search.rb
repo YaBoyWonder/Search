1e5e13afa6e662f1d70d3eb39e63bac92194a3ae
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-05-25 20:48:10 -0400

    Update search.rb

--- search/search.rb
+++ search/search.rb
@@ -6,7 +6,7 @@ playlist = youtube.add_playlist(:title => "My Playlist" )
 file = File.new( "keywords.txt", "r" )
 while( keyword = file.gets )
   results = youtube.videos_by( :query => keyword )
-   
+    
   if( results.total_result_count > 0 )
     youtube.add_video_to_playlist( playlist.playlist_id, results.videos[0].video_id )
   else
