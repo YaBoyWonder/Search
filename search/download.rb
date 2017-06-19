2f9b879f72355b5eb231e7424550a9de852b4ac2
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-06-19 18:52:13 -0400

    Create download.rb

---
+++ search/download.rb
@@ -0,0 +1,10 @@
+require 'viddl-rb'
+require 'open-uri'
+
+url = ViddlRb.get_urls('https://www.youtube.com/watch?v=zs7xO5P3Az4').first
+
+open('my_video.avi', 'wb') do |file|
+  file << open(url).read
+end
+
+puts Dir['./*.avi'] #=> ./my_video.avi
