18ef82f618564e4c54761e9a4f654be3d7decd3b
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-07-26 10:02:33 -0400

    Update download.rb

--- search/download.rb
+++ search/download.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 url = ViddlRb.get_urls('https://www.youtube.com/watch?v=zs7xO5P3Az4').first
 
-open('my_video.avi', 'wb') do |file|
+open('my_video.avi', 'wb') do |file| #kill necon
   file << open(url).read
 end
 
